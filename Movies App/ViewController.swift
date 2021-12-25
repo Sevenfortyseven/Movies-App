61c9d0eac4187feb521a5d50b728b9c5b317e6c7
aleksandre <alekokhubuluri1994@gmail.com>
2021-12-25 22:23:11 +0400

    starting point

---
+++ Movies App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movies App
+//
+//  Created by aleksandre on 25.12.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
